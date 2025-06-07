312dac3b993c1030779cd869cde6b2163e2df95f
sduddla <sduddla921@gmail.com>
2025-06-08 00:49:45 +0900

    🎨 Design: 프로필 퍼블리싱(border 제거)

--- src/components/mypage/MyProfile.vue
+++ src/components/mypage/MyProfile.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 <template>
   <!-- 프로필 전체 -->
-  <div class="flex items-center text-[#222222] mt-[30px] pl-[35px] border border-red-500">
+  <div class="flex items-center text-[#222222] mt-[30px] pl-[35px]">
     <!-- 이미지 -->
     <img
       src="https://images.unsplash.com/photo-1471115853179-bb1d604434e0?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fGNhbXBpbmd8ZW58MHx8MHx8fDA%3D"
