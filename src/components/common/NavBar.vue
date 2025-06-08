b18e611fa756683115561690dc1e6f0266967112
mjlee <leeminji9938@naver.com>
2025-06-09 03:05:39 +0900

    🎨 Design: Nav Bar 퍼블리싱

---
+++ src/components/common/NavBar.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+<template>
+  <section
+    class="w-full h-[60px] bg-[#4B3C2F] bottom-0 absolute px-8 flex items-center justify-between"
+  >
+    <div class="flex flex-col items-center cursor-pointer">
+      <img src="@/assets/icons/nav/nav-home-off.svg" alt="홈" class="w-6 h-6" />
+      <p class="text-[rgba(255,255,255,0.3)] text-sm mt-1">홈</p>
+    </div>
+    <div class="flex flex-col items-center cursor-pointer">
+      <img src="@/assets/icons/nav/nav-search-off.svg" alt="검색" class="w-6 h-6" />
+      <p class="text-[rgba(255,255,255,0.3)] text-sm mt-1">검색</p>
+    </div>
+    <div class="flex flex-col items-center cursor-pointer">
+      <img src="@/assets/icons/nav/nav-community-on.svg" alt="커뮤니티" class="w-6 h-6" />
+      <p class="text-[rgba(255,255,255,1)] text-sm mt-1">커뮤니티</p>
+    </div>
+    <div class="flex flex-col items-center cursor-pointer">
+      <img src="@/assets/icons/nav/nav-mypage-off.svg" alt="마이" class="w-6 h-6" />
+      <p class="text-[rgba(255,255,255,0.3)] text-sm mt-1">마이</p>
+    </div>
+  </section>
+</template>
+<style scoped></style>
