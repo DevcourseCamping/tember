b4c3edea58569e81841f95bb719c3723550338f7
mjlee <leeminji9938@naver.com>
2025-06-10 02:13:07 +0900

    ♻️  Refactor: 컴포넌트명 수정

--- src/components/common/HeaderSimple.vue
+++ src/components/common/HeaderSimple.vue
@@ -2,34 +2,34 @@
 import logo from '@/assets/icons/logo-brown.svg'
 import close from '@/assets/icons/close-brown.svg'
 import back from '@/assets/icons/light/light-back-header.svg'
-import more from '@/assets/icons/light/light-more.svg'
-import setting from '@/assets/icons/light/light-setting.svg'
-
 const props = defineProps({
   navType: {
     type: String, // close, back
     default: 'close',
   },
-  menuType: {
-    type: String, // more, setting
-    default: 'more',
+  centerContent: {
+    type: String, // title, logo
+    default: 'title',
+  },
+  title: {
+    type: String,
   },
 })
-const emit = defineEmits(['navClick', 'menuClick'])
+const emit = defineEmits(['navClick'])
 </script>
-
 <template>
-  <header>
-    <div class="w-full h-[80px] flex items-center px-[30px] justify-between">
-      <button @click="emit('navClick')">
-        <img v-if="props.navType === 'close'" :src="close" class="w-5 h-5" />
-        <img v-else :src="back" class="w-7 h-7" />
-      </button>
-      <img :src="logo" class="w-[84px] h-[43px]" />
-      <button @click="emit('menuClick')">
-        <img v-if="props.menuType === 'more'" :src="more" class="w-5 h-5" />
-        <img v-else :src="setting" class="w-7 h-7" />
-      </button>
+  <header class="relative h-20 bg-[var(--white)] flex items-center">
+    <button @click="emit('navClick')" class="absolute left-[30px]">
+      <img v-if="props.navType === 'close'" :src="close" class="w-5 h-5" />
+      <img v-else :src="back" class="w-7 h-7" />
+    </button>
+    <div class="absolute left-1/2 transform -translate-x-1/2">
+      <span v-if="props.centerContent === 'title'" class="text-xl">{{ props.title }}</span>
+      <img v-if="props.centerContent === 'logo'" :src="logo" class="w-[85px] h-[43px]" />
     </div>
   </header>
+
+  <!-- user profile page header 사용 시 -->
+  <!-- <HeaderSimple center-content="logo" nav-type="back" /> -->
 </template>
+<style scoped></style>
