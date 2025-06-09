408c86779f30df28a6df130283afe87626a0fab2
mjlee <leeminji9938@naver.com>
2025-06-09 23:25:19 +0900

    🎨 Design: Header Seach 컴포넌트 수정

--- src/components/common/HeaderSearch.vue
+++ src/components/common/HeaderSearch.vue
@@ -1,30 +1,34 @@
 <script setup>
+import { defineProps } from 'vue'
 import logo from '@/assets/icons/logo-white.svg'
 import filter from '@/assets/icons/light/light-filter.svg'
-//import arrow from '@/assets/icons/light/light-arrow-down.svg'
+import arrow from '@/assets/icons/light/light-arrow-down.svg'
 
+const props = defineProps({
+  iconType: { type: String, default: 'filter' },
+  onClick: { type: Function, default: () => {} },
+})
 </script>
+
 <template>
-  <header class="w-[500px] h-[188px] bg-[--primary] mx-auto flex flex-col items-center justify-center">
-    <img :src="logo" class="w-[106px] h-[53px]" />
-  <div class="relative w-[440px] h-[45px] flex items-center justify-between bg-[--white] rounded-md px-4 mt-7">
-  <img src="@/assets/icons/light/light-search.svg" class="w-5 h-5 mr-4" />
-  <input
-    type="text"
-    placeholder="검색하기"
-    class="flex-1 h-full focus:outline-none"
-  />
-  <img
-    :src="filter"
-    class="absolute right-[25px] w-5 h-5"
-  />
-  <!--<img
-    :src="arrow"
-    class="absolute right-[25px] w-5 h-5"
-  />-->
-  </div>
-</header>
+  <header class="w-[500px] bg-[var(--primary)] flex flex-col items-center pt-5 py-[30px] pb-[30px]">
+    <img :src="logo" class="w-[106px] h-[53px] mb-5" />
+    <div
+      class="relative w-[440px] h-[45px] flex items-center justify-between bg-[var(--white)] rounded-[5px] px-5 py-3"
+    >
+      <div class="flex items-center">
+        <img src="@/assets/icons/light/light-search.svg" class="w-5 h-5 mr-[30px]" />
+        <input
+          type="text"
+          placeholder="검색하기"
+          class="h-full focus:outline-none placeholder:text-[var(--grey)] text-[15px]"
+        />
+      </div>
+      <img
+        :src="props.iconType === 'arrow' ? arrow : filter"
+        class="absolute right-[25px] w-5 h-5 cursor-pointer"
+        @click="props.onClick"
+      />
+    </div>
+  </header>
 </template>
-<style scoped>
-  
-</style>
