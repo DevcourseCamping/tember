109d446a072997d35f4279f59b3ddae61505e2c2
Ujin <tladbwls0430@gmail.com>
2025-06-09 16:43:55 +0900

    헤더 퍼블리싱

---
+++ src/components/common/HeaderOther.vue
@@ -0,0 +1,18 @@
+<script setup>
+import logo from '@/assets/icons/logo-brown.svg'
+import close from '@/assets/icons/close-brown.svg'
+import more from '@/assets/icons/light/light-more.svg'
+//import setting from '@/assets/icons/light/light-setting.svg'
+</script>
+<template>
+  <header class="w-[500px] h-[80px] bg-[#FFFFFF] mx-auto flex flex-row items-center justify-between">
+    <img :src="close" class="w-[20px] h-[20px]" />
+    <img :src="logo" class="w-[84px] h-[43px]" />
+    <!--<p class="text-lg">리뷰쓰기</p>-->
+    <img :src="more" class="w-[20px] h-[20px]" />
+    <!--<img :src="setting" class="w-[20px] h-[20px]" />-->
+    </header>
+</template>
+<style scoped>
+  
+</style>
---
+++ src/components/common/HeaderSearchMain.vue
@@ -0,0 +1,53 @@
+<script setup>
+import logo from '@/assets/icons/logo-white.svg'
+import filter from '@/assets/icons/light/light-filter.svg'
+import search from '@/assets/icons/light/light-search.svg'
+import auto from '@/assets/icons/light/light-auto-camping-button.svg'
+import glamping from '@/assets/icons/light/light-glamping-button.svg'
+import caravan from '@/assets/icons/light/light-caravan-button.svg'
+import pet from '@/assets/icons/light/light-pet-button.svg'
+import chatbot from '@/assets/icons/chatbot-white.svg'
+
+</script>
+<template>
+  <header class="w-[500px] h-[253px] bg-[#4B3C2F] mx-auto flex flex-col items-center justify-center">
+    <img :src="logo" class="w-[106px] h-[53px]" />
+  <div class="relative w-[440px] h-[45px] flex items-center justify-between bg-white rounded-md px-4 mt-7">
+  <img :src="search" class="w-5 h-5 mr-4" />
+  <input
+    type="text"
+    placeholder="검색하기"
+    class="flex-1 h-full focus:outline-none"
+  />
+  <img
+    :src="filter"
+    class="absolute right-[25px] w-5 h-5"
+  />
+  </div>
+  <div class="flex space-x-[54px] mt-6 w-[440px] h-[70px] mx-auto justify-center">
+  <div class="flex flex-col items-center">
+    <img :src="auto" class="w-[45px] h-[45px]" />
+    <span class="text-xs mt-1 text-white whitespace-nowrap">오토캠핑</span>
+  </div>
+  <div class="flex flex-col items-center">
+    <img :src="glamping" class="w-[45px] h-[45px]" />
+    <span class="text-xs mt-1 text-white">글램핑</span>
+  </div>
+  <div class="flex flex-col items-center">
+    <img :src="caravan" class="w-[45px] h-[45px]" />
+    <span class="text-xs mt-1 text-white">카라반</span>
+  </div>
+  <div class="flex flex-col items-center">
+    <img :src="pet" class="w-[45px] h-[45px]" />
+    <span class="text-xs mt-1 text-white whitespace-nowrap">반려동물</span>
+  </div>
+  <div class="flex flex-col items-center">
+    <img :src="chatbot" class="w-[45px] h-[45px]" />
+    <span class="text-xs mt-1 text-white">챗봇</span>
+  </div>
+</div>
+</header>
+</template>
+<style scoped>
+  
+</style>
