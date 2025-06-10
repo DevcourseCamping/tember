f05511a396c554322f5bd2814a86203cca2493e8
Ujin <tladbwls0430@gmail.com>
2025-06-10 09:41:50 +0900

    로딩 퍼블리싱

---
+++ src/components/common/LoadingVue.vue
@@ -0,0 +1,13 @@
+<script setup>
+import logo from '@/assets/icons/logo-white.svg'
+</script>
+<template>
+  <div class="w-[500px] h-screen bg-[var(--primary)] mx-auto relative flex justify-center items-center">
+  <img 
+    :src="logo"
+    class="w-[136px] h-[64px]"
+  />
+</div>
+</template>
+<style scoped>
+</style>
