6bae5ddf8968416db0b155674a10955e0e24efeb
sduddla <sduddla921@gmail.com>
2025-06-12 00:51:43 +0900

    페이지 이동 연결

--- src/views/mypage/MyPage.vue
+++ src/views/mypage/MyPage.vue
@@ -1,16 +1,29 @@
 <script setup>
+import BottomSheet from '@/components/common/BottomSheet.vue'
 import HeaderOther from '@/components/common/HeaderOther.vue'
 import MyActivity from '@/components/mypage/MyActivity.vue'
 import MyProfile from '@/components/mypage/MyProfile.vue'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const isBottomOpen = ref(false)
+
 const clickBack = () => {
-  console.log('back')
+  router.push({ name: 'home' })
 }
 const clickSetting = () => {
-  console.log('setting')
+  isBottomOpen.value = !isBottomOpen.value
+}
+
+const handleSelect = (key) => {
+  if (key === 'edit') {
+    router.push({ name: 'edit' })
+  }
 }
 </script>
 <template>
-  <div class="fixed w-[500px] h-screen bg-white left-1/2 -translate-x-1/2">
+  <div class="fixed w-full max-w-[500px] h-screen bg-white left-1/2 -translate-x-1/2">
     <!-- header -->
     <HeaderOther
       nav-type="back"
@@ -18,10 +31,14 @@ const clickSetting = () => {
       @navClick="clickBack"
       @menuClick="clickSetting"
     />
+
+    <BottomSheet v-if="isBottomOpen" type="my" @close="clickSetting" @select="handleSelect" />
+
     <MyProfile />
     <MyActivity />
     <!-- footer -->
     <button
+      v-if="!isBottomOpen"
       href="#"
       class="absolute bottom-0 w-full h-[60px] bg-[#4B3C2F] flex justify-center items-center text-[18px] text-[#FFFFFF]"
     >
