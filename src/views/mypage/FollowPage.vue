3e4332fce93c996b4f296ba4eb898be0978d647b
sduddla <sduddla921@gmail.com>
2025-06-12 00:43:26 +0900

    팔로우 페이지에서 마이페이지로 이동 연결

--- src/views/mypage/FollowPage.vue
+++ src/views/mypage/FollowPage.vue
@@ -1,11 +1,18 @@
 <script setup>
 import HeaderSimple from '@/components/common/HeaderSimple.vue'
 import FollowList from '@/components/mypage/FollowList.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const clickClose = () => {
+  router.go(-1)
+}
 </script>
 <template>
   <div class="fixed w-[500px] h-screen bg-[var(--white)] left-1/2 -translate-x-1/2">
     <!-- header -->
-    <HeaderSimple title="캠핑 초보" />
+    <HeaderSimple title="캠핑 초보" @click="clickClose" />
     <!-- main -->
     <FollowList />
   </div>
