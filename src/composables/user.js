624af94585a08cf832f9ffe377612a4b52b1fdfb
sduddla <sduddla921@gmail.com>
2025-06-12 00:47:50 +0900

    데이터 가져오는 API 연결

---
+++ src/composables/user.js
@@ -0,0 +1,114 @@
+import supabase from '@/utils/supabase'
+import axios from 'axios'
+
+const userApi = axios.create({
+  baseURL: import.meta.env.VITE_SUPABASE_URL + '/functions/v1',
+})
+
+export function useUserApi() {
+  const getUser = async (user_id) => {
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.getSession()
+    if (error || !session) throw new Error('no 세션')
+
+    const token = session.access_token
+
+    const res = await userApi.get(`/profile/${user_id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    const {
+      data: { user, followerCount, followingCount },
+    } = res.data
+
+    return {
+      user,
+      followingCount,
+      followerCount,
+    }
+  }
+
+  const updateUser = async ({ userId, username, image }) => {
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.getSession()
+
+    if (error || !session) throw new Error('no 세션')
+
+    const token = session.access_token
+
+    const formData = new FormData()
+
+    formData.append('userId', userId)
+    formData.append('username', username)
+    formData.append('image', image)
+
+    const res = await userApi.patch(`/update-profile`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return res.data
+  }
+
+  const getPost = async (user_id) => {
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.getSession()
+
+    if (error || !session) throw new Error('no 세션')
+
+    const token = session.access_token
+
+    const res = await userApi.get(`/get-user-posts/${user_id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    return res.data
+  }
+
+  const getBookmark = async (user_id) => {
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.getSession()
+
+    if (error || !session) throw new Error('no 세션')
+
+    const token = session.access_token
+
+    const res = await userApi.get(`/get-bookmark/${user_id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return res.data.bookmark
+  }
+
+  const getReview = async (content_id) => {
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.getSession()
+
+    if (error || !session) throw new Error('no 세션')
+    const token = session.access_token
+
+    const res = await userApi.get(`/get-campinfo/${content_id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return res.data
+  }
+
+  return { getUser, updateUser, getPost, getBookmark, getReview }
+}
