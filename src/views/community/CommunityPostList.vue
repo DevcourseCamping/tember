c60c2827d6060a88ea8319ed5d651b7f270a1078
Ujin <tladbwls0430@gmail.com>
2025-06-12 09:52:32 +0900

    최신순 인기순 바텀시트 추가

--- src/views/community/CommunityPostList.vue
+++ src/views/community/CommunityPostList.vue
@@ -3,11 +3,23 @@ import PostCard from '@/components/community/PostCard.vue'
 import HeaderSearch from '@/components/common/HeaderSearch.vue'
 import NavBar from '@/components/common/NavBar.vue'
 import post from '@/assets/icons/light/light-post-opacity.svg'
+import BottomSheet from '@/components/common/BottomSheet.vue'
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 // bottom sheet 완성되면 마무리
+const isBottomOpen = ref(false)
 const openBottomSheet = () => {
+  isBottomOpen.value = true
   console.log('🧸 Open Bottom Sheet')
+}
+const handleSelect = (key) => {
+  console.log('정렬 선택:', key)
+  isBottomOpen.value = false
+}
+
+const closeSheet = () => {
+  isBottomOpen.value = false
 }
 const router = useRouter()
 const goToCreatePost = () => {
@@ -32,5 +44,18 @@ const goToCreatePost = () => {
       <img :src="post" />
     </div>
   </div>
+    <Teleport to="body">
+   <div
+     v-if="isBottomOpen"
+     class="fixed bottom-0 left-1/2 transform -translate-x-1/2
+             w-full max-w-[500px] z-50"
+   >
+     <BottomSheet
+       type="sort"
+       @select="handleSelect"
+       @close="closeSheet"
+     />
+   </div>
+  </Teleport>
 </template>
 <style scoped></style>
