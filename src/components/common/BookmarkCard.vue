858547c7cf3ecb7eb4a407ea20f2385ec75737b9
sduddla <sduddla921@gmail.com>
2025-06-12 09:44:24 +0900

    북마크한 캠핑장 없을 때 문구 추가

--- src/components/common/BookmarkCard.vue
+++ src/components/common/BookmarkCard.vue
@@ -39,53 +39,61 @@ onMounted(async () => {
 })
 </script>
 <template>
-  <div class="p-[30px] flex flex-col gap-[30px]">
+  <div class="p-[30px] flex flex-col">
     <div
-      v-for="item in bookmarks"
-      :key="item.camp_id"
-      class="relative border border-[var(--primary-30)] rounded-[5px] cursor-pointer"
+      v-if="bookmarks.length === 0"
+      class="text-center text-[var(--grey)] text-[14px] flex items-center justify-center h-[calc(100vh-450px)]"
     >
-      <img
-        v-if="item.camp_sites.first_image_url"
-        :src="item.camp_sites.first_image_url"
-        alt="임시 캠핑장 이미지"
-        class="w-[440px] h-[216px] rounded-tl-[5px] rounded-tr-[5px]"
-      />
-      <div class="flex flex-col pl-[15px]">
-        <div class="flex items-center justify-between pt-[15px]">
-          <p class="text-[14px] text-[var(--black)]">{{ item.camp_sites.induty }}</p>
-          <img
-            :src="item.isMarked ? filledBookmarkIcon : outlineBookmarkIcon"
-            alt="북마크"
-            class="pr-[15px]"
-          />
-        </div>
+      북마크한 캠핑장이 없습니다.
+    </div>
+    <div v-else>
+      <div
+        v-for="item in bookmarks"
+        :key="item.camp_id"
+        class="mb-[30px] relative border border-[var(--primary-30)] rounded-[5px] cursor-pointer"
+      >
+        <img
+          v-if="item.camp_sites.first_image_url"
+          :src="item.camp_sites.first_image_url"
+          alt="임시 캠핑장 이미지"
+          class="w-[440px] h-[216px] rounded-tl-[5px] rounded-tr-[5px]"
+        />
+        <div class="flex flex-col pl-[15px]">
+          <div class="flex items-center justify-between pt-[15px]">
+            <p class="text-[14px] text-[var(--black)]">{{ item.camp_sites.induty }}</p>
+            <img
+              :src="item.isMarked ? filledBookmarkIcon : outlineBookmarkIcon"
+              alt="북마크"
+              class="pr-[15px]"
+            />
+          </div>
 
-        <p class="font-semibold text-[17px]">{{ item.camp_sites.faclt_nm }}</p>
-        <p class="text-[var(--grey)] pt-[5px] text-[15px]">{{ item.camp_sites.sigungu_nm }}</p>
+          <p class="font-semibold text-[17px]">{{ item.camp_sites.faclt_nm }}</p>
+          <p class="text-[var(--grey)] pt-[5px] text-[15px]">{{ item.camp_sites.sigungu_nm }}</p>
 
-        <div class="flex gap-[20px] pb-[20px] pt-[20px]">
-          <img
-            :src="item.filter?.caravan ? onCaravanIcon : offCaravanIcon"
-            alt="카라반"
-            class="w-[24px] h-[24px]"
-          />
-          <img
-            :src="item.filter?.trailer ? onTrailerIcon : offTrailerIcon"
-            alt="트레일러"
-            class="w-[24px] h-[24px]"
-          />
-          <img
-            :src="item.filter?.pet ? onPetIcon : offPetIcon"
-            alt="반려동물"
-            class="w-[24px] h-[24px]"
-          />
-        </div>
-        <div
-          class="absolute right-0 bottom-0 w-[100px] h-[36px] bg-[var(--primary)] flex items-center justify-center gap-[13px] rounded-tl-[5px] rounded-br-[5px]"
-        >
-          <p class="text-[var(--white)]">4.8</p>
-          <img :src="filledStarIcon" alt="별점" class="w-[20px] h-[20px]" />
+          <div class="flex gap-[20px] pb-[20px] pt-[20px]">
+            <img
+              :src="item.filter?.caravan ? onCaravanIcon : offCaravanIcon"
+              alt="카라반"
+              class="w-[24px] h-[24px]"
+            />
+            <img
+              :src="item.filter?.trailer ? onTrailerIcon : offTrailerIcon"
+              alt="트레일러"
+              class="w-[24px] h-[24px]"
+            />
+            <img
+              :src="item.filter?.pet ? onPetIcon : offPetIcon"
+              alt="반려동물"
+              class="w-[24px] h-[24px]"
+            />
+          </div>
+          <div
+            class="absolute right-0 bottom-0 w-[100px] h-[36px] bg-[var(--primary)] flex items-center justify-center gap-[13px] rounded-tl-[5px] rounded-br-[5px]"
+          >
+            <p class="text-[var(--white)]">4.8</p>
+            <img :src="filledStarIcon" alt="별점" class="w-[20px] h-[20px]" />
+          </div>
         </div>
       </div>
     </div>
