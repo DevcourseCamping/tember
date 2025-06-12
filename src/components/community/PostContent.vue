18fc7cab834cf6182609ccb3a31ba379ee181aa3
mjlee <leeminji9938@naver.com>
2025-06-13 00:05:09 +0900

    게시글 수정 기능 구현

---
+++ src/components/community/PostContent.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Pagination } from 'swiper/modules'
+
+defineProps({
+  post: Object,
+  imageList: Array,
+})
+</script>
+
+<template>
+  <section v-if="imageList.length" class="w-full mt-4">
+    <Swiper :modules="[Pagination]" :pagination="{ clickable: true }" class="w-full h-[480px]">
+      <SwiperSlide v-for="(img, idx) in imageList" :key="idx">
+        <img :src="img" class="w-full h-full object-cover cursor-pointer" />
+      </SwiperSlide>
+    </Swiper>
+  </section>
+  <p class="px-5 mt-6">{{ post.content }}</p>
+</template>
