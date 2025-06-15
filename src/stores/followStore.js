7f807de77fc355a82c93e5d5aae362c1ff0927ca
sduddla <sduddla921@gmail.com>
2025-06-15 18:08:35 +0900

    팔로우 스토어 생성

--- src/stores/followStore.js
+++ src/stores/followStore.js
@@ -9,7 +9,7 @@ export const useFollowStore = defineStore('followStore', {
   }),
 
   actions: {
-    async followUser(myId, userId) {
+    async followUser(myId, userId, list = 'followers') {
       const { data: exist } = await supabase
         .from('follows')
         .select('id')
@@ -27,31 +27,55 @@ export const useFollowStore = defineStore('followStore', {
         },
       ])
       if (!error) this.isFollowing = true
+
+      await this.fetchFollowing(myId)
+
+      if (list === 'followers') {
+        await this.fetchFollowers(myId)
+      }
     },
 
-    async unfollowUser(myId, userId) {
+    async unfollowUser(myId, userId, list = 'followings') {
       const { error } = await supabase
         .from('follows')
         .delete()
         .eq('follower_id', myId)
         .eq('following_id', userId)
 
-      if (!error) this.isFollowing = false
+      if (error) return
+      this.isFollowing = false
+
+      if (list === 'followings') {
+        this.followings = this.followings.filter((item) => item.following_id !== userId)
+        const target = this.followers.find((item) => item.follower_id === userId)
+        if (target) target.isFollow = false
+      } else if (list === 'followers') {
+        const target = this.followers.find((item) => item.follower_id === userId)
+        if (target) target.isFollow = false
+      }
+
+      await this.fetchFollowing(myId)
     },
 
     async fetchFollowers(userId) {
       const { data, error } = await supabase
         .from('follows')
-        .select('follower_id, profiles(*)')
+        .select('follower_id, profiles: follower_id(*)')
         .eq('following_id', userId)
 
-      if (!error) this.followers = data
+      if (error) return
+
+      const MyFollowing = this.followings.map((item) => item.following_id)
+      this.followers = data.map((item) => ({
+        ...item,
+        isFollow: MyFollowing.includes(item.follower_id),
+      }))
     },
 
     async fetchFollowing(userId) {
       const { data, error } = await supabase
         .from('follows')
-        .select('following_id, profiles(*)')
+        .select('following_id, profiles: following_id(*)')
         .eq('follower_id', userId)
 
       if (!error) this.followings = data
