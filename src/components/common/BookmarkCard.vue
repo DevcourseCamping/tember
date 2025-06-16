9dfc29e6e13f86b4c5969aa811f06f850afb204d
Ujin <tladbwls0430@gmail.com>
2025-06-16 09:19:40 +0900

    검색 완

--- src/components/common/BookmarkCard.vue
+++ src/components/common/BookmarkCard.vue
@@ -1,18 +1,10 @@
 <script setup>
-import filledBookmarkIcon from '../../assets/icons/light/light-bookmark-filled.svg'
-import outlineBookmarkIcon from '../../assets/icons/light/light-bookmark-outline.svg'
-
-import onCaravanIcon from '../../assets/icons/light/light-caravan-on.svg'
-import offCaravanIcon from '../../assets/icons/light/light-caravan-off.svg'
-import onPetIcon from '../../assets/icons/light/light-pet-on.svg'
-import offPetIcon from '../../assets/icons/light/light-pet-off.svg'
-import onTrailerIcon from '../../assets/icons/light/light-trailer-on.svg'
-import offTrailerIcon from '../../assets/icons/light/light-trailer-off.svg'
-import filledStarIcon from '../../assets/icons/star-filled.svg'
 import { useUserStore } from '@/stores/userStore'
 import { onMounted, ref } from 'vue'
 import { useUserApi } from '@/composables/useUserApi'
 import BookmarkSkeleton from '../mypage/BookmarkSkeleton.vue'
+import ThemeIcon from '@/components/common/ThemeIcon.vue'
+import filledStarIcon from '../../assets/icons/star-filled.svg'
 
 const profile = useUserStore()
 const bookmarks = ref([])
@@ -50,6 +42,7 @@ onMounted(async () => {
   }
 })
 </script>
+
 <template>
   <div class="p-[30px] flex flex-col">
     <div v-if="isLoading">
@@ -65,39 +58,39 @@ onMounted(async () => {
       <div
         v-for="item in bookmarks"
         :key="item.camp_id"
-        class="mb-[30px] relative border border-[var(--primary-30)] rounded-[5px] cursor-pointer"
+        class="mb-[30px] relative bg-[var(--color-surface)] rounded-[5px] cursor-pointer"
       >
         <img
           :src="item.camp_sites.first_image_url ? item.camp_sites.first_image_url : defaultImage"
           alt="임시 캠핑장 이미지"
-          class="w-[440px] h-[216px] rounded-tl-[5px] rounded-tr-[5px]"
+          class="w-full h-[216px] rounded-tl-[5px] rounded-tr-[5px] object-cover"
         />
         <div class="flex flex-col pl-[15px]">
           <div class="flex items-center justify-between pt-[15px]">
-            <p class="text-[14px] text-[var(--black)]">{{ item.camp_sites.induty }}</p>
-            <img
-              :src="item.isMarked ? filledBookmarkIcon : outlineBookmarkIcon"
+            <p class="text-[13px] text-[var(--color-text-primary)]">{{ item.camp_sites.induty }}</p>
+            <ThemeIcon
+              :name="item.isMarked ? 'bookmark-filled' : 'bookmark-outline'"
               alt="북마크"
-              class="pr-[15px]"
+              class="pr-[15px] w-[39px] h-[24px]"
             />
           </div>
 
-          <p class="font-semibold text-[17px]">{{ item.camp_sites.faclt_nm }}</p>
-          <p class="text-[var(--grey)] pt-[5px] text-[15px]">{{ item.camp_sites.sigungu_nm }}</p>
+          <p class="font-semibold text-[19px]">{{ item.camp_sites.faclt_nm }}</p>
+          <p class="text-[var(--grey)] pt-[5px] text-[13px]">{{ item.camp_sites.sigungu_nm }}</p>
 
           <div class="flex gap-[20px] pb-[20px] pt-[20px]">
-            <img
-              :src="item.filter?.caravan ? onCaravanIcon : offCaravanIcon"
+            <ThemeIcon
+              :name="item.filter?.caravan ? 'caravan-on' : 'caravan-off'"
               alt="카라반"
               class="w-[24px] h-[24px]"
             />
-            <img
-              :src="item.filter?.trailer ? onTrailerIcon : offTrailerIcon"
+            <ThemeIcon
+              :name="item.filter?.trailer ? 'trailer-on' : 'trailer-off'"
               alt="트레일러"
               class="w-[24px] h-[24px]"
             />
-            <img
-              :src="item.filter?.pet ? onPetIcon : offPetIcon"
+            <ThemeIcon
+              :name="item.filter?.pet ? 'pet-on' : 'pet-off'"
               alt="반려동물"
               class="w-[24px] h-[24px]"
             />
@@ -105,7 +98,7 @@ onMounted(async () => {
           <div
             class="absolute right-0 bottom-0 w-[100px] h-[36px] bg-[var(--primary)] flex items-center justify-center gap-[13px] rounded-tl-[5px] rounded-br-[5px]"
           >
-            <p class="text-[var(--white)]">4.8</p>
+            <p class="text-[var(--color-text-on-primary)]">4.8</p>
             <img :src="filledStarIcon" alt="별점" class="w-[20px] h-[20px]" />
           </div>
         </div>
--- src/components/common/HeaderSearch.vue
+++ src/components/common/HeaderSearch.vue
@@ -1,16 +1,14 @@
 <script setup>
 import logo from '@/assets/icons/logo-white.svg'
-import filter from '@/assets/icons/light/light-filter.svg'
-import arrow from '@/assets/icons/light/light-arrow-down.svg'
-import search from '@/assets/icons/light/light-search.svg'
+import ThemeIcon from '@/components/common/ThemeIcon.vue'
 
-const props = defineProps({
+/* const props = defineProps({
   iconType: {
     type: String,
-    default: 'filter', // filter, arrow
+    default: 'filter',
   },
-})
-const emit = defineEmits(['iconClick'])
+}) */
+const emit = defineEmits(['filterClick'])
 </script>
 
 <template>
@@ -18,20 +16,22 @@ const emit = defineEmits(['iconClick'])
     class="bg-[var(--primary)] w-full max-w-[500px] flex flex-col items-center pt-5 px-[30px] pb-[30px]"
   >
     <img :src="logo" class="w-[106px] h-[53px] mb-5" />
-    <div
-      class="relative h-[45px] flex items-center justify-between bg-[var(--white)] rounded-[5px] px-5 py-3"
-    >
-      <div class="flex items-center">
-        <img :src="search" class="w-5 h-5 mr-[30px]" />
-        <input
-          type="text"
-          placeholder="검색하기"
-          class="w-[330px] h-full focus:outline-none placeholder:text-[var(--grey)] text-[15px]"
-        />
-        <button @click="emit('iconClick')">
-          <img :src="props.iconType === 'arrow' ? arrow : filter" class="w-5 h-5" />
-        </button>
+    <section class="px-[30px]">
+      <div
+        class="relative h-[45px] flex items-center justify-between bg-[var(--popular)] rounded-[5px] px-5 py-3"
+      >
+        <div class="flex items-center w-full">
+          <ThemeIcon name="search" class="w-5 h-5 mr-[30px]" />
+          <input
+            type="text"
+            placeholder="검색하기"
+            class="w-[330px] h-full focus:outline-none placeholder:text-[var(--grey)] text-[15px] bg-[var(--popular)]"
+          />
+          <button @click="emit('filterClick')">
+            <ThemeIcon name="filter" class="w-5 h-5" />
+          </button>
+        </div>
       </div>
-    </div>
-  </header>
+    </section>
+    </header>
 </template>
