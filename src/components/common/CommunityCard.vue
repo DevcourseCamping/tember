e8e9d55d2de21717f6c3d9566f9f316daf09e9b1
sduddla <sduddla921@gmail.com>
2025-06-16 15:06:00 +0900

    formDate 컴포넌트 사용

--- src/components/common/CommunityCard.vue
+++ src/components/common/CommunityCard.vue
@@ -7,24 +7,12 @@ import { useUserStore } from '@/stores/userStore'
 import { useUserApi } from '@/composables/useUserApi'
 import CommunitySkeleton from '../mypage/CommunitySkeleton.vue'
 import { useUserPage } from '@/composables/useUserPage'
+import formDate from '@/utils/formDate'
 
 const profile = useUserStore()
 const posts = ref([])
 const isLoading = ref(true)
 const { targetUserId } = useUserPage()
-
-const formDate = (value) => {
-  const now = new Date()
-  const timeValue = new Date(value)
-
-  const time = Math.floor(now.getTime() - timeValue.getTime()) / 1000
-  if (time < 60) return '방금 전'
-  if (time < 3600) return `${Math.floor(time / 60)}분 전`
-  if (time < 86400) return `${Math.floor(time / 3600)}시간 전`
-  if (time < 31536000) return `${Math.floor(time / 86400)}일 전`
-
-  return `${Math.floor(time / 31536000)}년 전`
-}
 
 onMounted(async () => {
   isLoading.value = true
