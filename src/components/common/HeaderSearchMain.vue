0504ebe8a091b3174c179ead8ff156a3d3c74d92
Ujin <tladbwls0430@gmail.com>
2025-06-17 23:42:29 +0900

    메인헤더 네브바 다크모드 적용, 챗봇 삭제

--- src/components/common/HeaderSearchMain.vue
+++ src/components/common/HeaderSearchMain.vue
@@ -1,18 +1,24 @@
 <script setup>
 import logo from '@/assets/icons/logo-white.svg'
 import filter from '@/assets/icons/light/light-filter.svg'
+/* import darkfilter from '@/assets/icons/dark/dark-filter.svg' */
 import search from '@/assets/icons/light/light-search.svg'
+/* import darksearch from '@/assets/icons/dark/dark-search.svg' */
 import auto from '@/assets/icons/light/light-auto-camping-button.svg'
+import darkauto from '@/assets/icons/dark/dark-auto-camping-button.svg'
 import glamping from '@/assets/icons/light/light-glamping-button.svg'
+import darkglamping from '@/assets/icons/dark/dark-glamping-button.svg'
 import caravan from '@/assets/icons/light/light-caravan-button.svg'
+import darkcaravan from '@/assets/icons/dark/dark-caravan-button.svg'
 import pet from '@/assets/icons/light/light-pet-button.svg'
-import chatbot from '@/assets/icons/chatbot-white.svg'
+import darkpet from '@/assets/icons/dark/dark-pet-button.svg'
+/* import chatbot from '@/assets/icons/chatbot-white.svg' */
 
 const emit = defineEmits(['filterClick', 'categoryClick'])
 </script>
 
 <template>
-  <header class="bg-[var(--primary)] flex flex-col items-center pt-5 py-[30px] pb-[25px]">
+  <header class="bg-[var(--primary)] flex flex-col items-center pt-5 py-[30px] pb-[25px] dark:bg-[#181818]">
     <img :src="logo" class="w-[106px] h-[53px] mb-5" />
     <section class="px-[30px]">
       <div
@@ -20,6 +26,10 @@ const emit = defineEmits(['filterClick', 'categoryClick'])
       >
         <div class="flex items-center w-full">
           <img :src="search" class="w-5 h-5 mr-[30px]" />
+          <!-- <img
+        :src="darksearch"
+        class="w-5 h-5 mr-[30px] hidden dark:block"
+      /> -->
           <input
             type="text"
             placeholder="검색하기"
@@ -27,31 +37,45 @@ const emit = defineEmits(['filterClick', 'categoryClick'])
           />
           <button @click="emit('filterClick')">
             <img :src="filter" class="w-5 h-5" />
+            <!-- <img :src="darkfilter" class="w-5 h-5 hidden dark:block" /> -->
           </button>
         </div>
       </div>
     </section>
-    <section class="flex w-full px-[30px] pt-[25px] justify-between">
+    <section class="flex w-full px-[30px] pt-[25px] justify-evenly gap-x-6">
       <button class="flex flex-col items-center" @click="emit('categoryClick', 'autoCamping')">
-        <img :src="auto" class="w-[45px] h-[45px]" />
+        <img :src="auto" class="w-[45px] h-[45px] block dark:hidden" />
+        <img
+    :src="darkauto"
+    class="w-[45px] h-[45px] hidden dark:block"
+  />
         <span class="text-sm mt-[5px] text-[var(--white)]">오토캠핑</span>
       </button>
       <button class="flex flex-col items-center" @click="emit('categoryClick', 'glamping')">
-        <img :src="glamping" class="w-[45px] h-[45px]" />
+        <img :src="glamping" class="w-[45px] h-[45px] block dark:hidden" />
+        <img
+    :src="darkglamping"
+    class="w-[45px] h-[45px] hidden dark:block"
+  />
         <span class="text-sm mt-[5px] text-[var(--white)]">글램핑</span>
       </button>
       <button class="flex flex-col items-center" @click="emit('categoryClick', 'caravan')">
-        <img :src="caravan" class="w-[45px] h-[45px]" />
+        <img :src="caravan" class="w-[45px] h-[45px] block dark:hidden" />
+        <img
+    :src="darkcaravan"
+    class="w-[45px] h-[45px] hidden dark:block"
+  />
         <span class="text-sm mt-[5px] text-[var(--white)]">카라반</span>
       </button>
       <button class="flex flex-col items-center" @click="emit('categoryClick', 'pet')">
-        <img :src="pet" class="w-[45px] h-[45px]" />
+        <img :src="pet" class="w-[45px] h-[45px] block dark:hidden" />
+        <img :src="darkpet" class="w-[45px] h-[45px] hidden dark:block" />
         <span class="text-sm mt-[5px] text-[var(--white)]">반려동물</span>
       </button>
-      <button class="flex flex-col items-center" @click="emit('categoryClick', 'chatbot')">
+      <!-- <button class="flex flex-col items-center" @click="emit('categoryClick', 'chatbot')">
         <img :src="chatbot" class="w-[45px] h-[45px]" />
         <span class="text-sm mt-[5px] text-[var(--white)]">챗봇</span>
-      </button>
+      </button> -->
     </section>
   </header>
 </template>
