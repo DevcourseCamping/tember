d7fffe0b6545cda6998cdb776febfc8120622188
Ujin <tladbwls0430@gmail.com>
2025-06-18 00:00:59 +0900

    인풋 검색 필터 다크모드

--- src/components/common/HeaderSearchMain.vue
+++ src/components/common/HeaderSearchMain.vue
@@ -1,9 +1,9 @@
 <script setup>
 import logo from '@/assets/icons/logo-white.svg'
 import filter from '@/assets/icons/light/light-filter.svg'
-/* import darkfilter from '@/assets/icons/dark/dark-filter.svg' */
+import darkfilter from '@/assets/icons/dark/dark-filter.svg'
 import search from '@/assets/icons/light/light-search.svg'
-/* import darksearch from '@/assets/icons/dark/dark-search.svg' */
+import darksearch from '@/assets/icons/dark/dark-search.svg'
 import auto from '@/assets/icons/light/light-auto-camping-button.svg'
 import darkauto from '@/assets/icons/dark/dark-auto-camping-button.svg'
 import glamping from '@/assets/icons/light/light-glamping-button.svg'
@@ -22,22 +22,22 @@ const emit = defineEmits(['filterClick', 'categoryClick'])
     <img :src="logo" class="w-[106px] h-[53px] mb-5" />
     <section class="px-[30px]">
       <div
-        class="relative h-[45px] flex items-center justify-between bg-[var(--white)] rounded-[5px] px-5 py-3"
+        class="relative h-[45px] flex items-center justify-between bg-[var(--white)] dark:bg-[#3a3a3a] rounded-[5px] px-5 py-3"
       >
-        <div class="flex items-center w-full">
-          <img :src="search" class="w-5 h-5 mr-[30px]" />
-          <!-- <img
+        <div class="flex items-center w-full ">
+          <img :src="search" class="w-5 h-5 mr-[30px] block dark:hidden" />
+          <img
         :src="darksearch"
         class="w-5 h-5 mr-[30px] hidden dark:block"
-      /> -->
+      />
           <input
             type="text"
             placeholder="검색하기"
-            class="w-[330px] h-full focus:outline-none placeholder:text-[var(--grey)] text-[15px]"
+            class="w-[330px] h-full focus:outline-none placeholder:text-[var(--grey)] text-[15px] dark:bg-[#3a3a3a]"
           />
           <button @click="emit('filterClick')">
-            <img :src="filter" class="w-5 h-5" />
-            <!-- <img :src="darkfilter" class="w-5 h-5 hidden dark:block" /> -->
+            <img :src="filter" class="w-5 h-5 block dark:hidden"/>
+            <img :src="darkfilter" class="w-5 h-5 hidden dark:block" />
           </button>
         </div>
       </div>
