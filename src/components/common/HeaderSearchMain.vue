65cfae196294f6be9d5bd4ac8969ed4a63c8db96
Ujin <tladbwls0430@gmail.com>
2025-06-18 09:36:57 +0900

    헤더 간격수정

--- src/components/common/HeaderSearchMain.vue
+++ src/components/common/HeaderSearchMain.vue
@@ -18,53 +18,56 @@ const emit = defineEmits(['filterClick', 'categoryClick'])
 </script>
 
 <template>
-  <header class="bg-[var(--primary)] flex flex-col items-center pt-5 py-[30px] pb-[25px] dark:bg-[#181818]">
-    <img :src="logo" class="w-[106px] h-[53px] mb-5" />
-    <section class="px-[30px]">
+  <header class="bg-[var(--primary)] flex flex-col items-center pt-5 pb-[22px] dark:bg-[#181818]">
+    
+    <img :src="logo" class="w-[106px] h-[53px]" />
+
+    <section class="w-full px-[22px] mt-[22px]">
       <div
-        class="relative h-[45px] flex items-center justify-between bg-[var(--white)] dark:bg-[#3a3a3a] rounded-[5px] px-5 py-3"
+        class="relative h-[45px] flex items-center bg-[var(--white)] dark:bg-[#3a3a3a] rounded-[5px] px-5"
       >
-        <div class="flex items-center w-full ">
+        <div class="flex items-center w-full">
           <img :src="search" class="w-5 h-5 mr-[30px] block dark:hidden" />
           <img
-        :src="darksearch"
-        class="w-5 h-5 mr-[30px] hidden dark:block"
-      />
+            :src="darksearch"
+            class="w-5 h-5 mr-[30px] hidden dark:block"
+          />
           <input
             type="text"
             placeholder="검색하기"
-            class="w-[330px] h-full focus:outline-none placeholder:text-[var(--grey)] text-[15px] dark:bg-[#3a3a3a]"
+            class="w-full h-full focus:outline-none placeholder:text-[var(--grey)] text-[15px] bg-transparent"
           />
           <button @click="emit('filterClick')">
-            <img :src="filter" class="w-5 h-5 block dark:hidden"/>
+            <img :src="filter" class="w-5 h-5 block dark:hidden" />
             <img :src="darkfilter" class="w-5 h-5 hidden dark:block" />
           </button>
         </div>
       </div>
     </section>
-    <section class="flex w-full px-[30px] pt-[25px] justify-evenly gap-x-6">
+
+    <section class="flex w-full px-[22px] mt-[23px] justify-center gap-x-[50px]">
       <button class="flex flex-col items-center" @click="emit('categoryClick', 'autoCamping')">
         <img :src="auto" class="w-[45px] h-[45px] block dark:hidden" />
         <img
-    :src="darkauto"
-    class="w-[45px] h-[45px] hidden dark:block"
-  />
+          :src="darkauto"
+          class="w-[45px] h-[45px] hidden dark:block"
+        />
         <span class="text-sm mt-[5px] text-[var(--white)]">오토캠핑</span>
       </button>
       <button class="flex flex-col items-center" @click="emit('categoryClick', 'glamping')">
         <img :src="glamping" class="w-[45px] h-[45px] block dark:hidden" />
         <img
-    :src="darkglamping"
-    class="w-[45px] h-[45px] hidden dark:block"
-  />
+          :src="darkglamping"
+          class="w-[45px] h-[45px] hidden dark:block"
+        />
         <span class="text-sm mt-[5px] text-[var(--white)]">글램핑</span>
       </button>
       <button class="flex flex-col items-center" @click="emit('categoryClick', 'caravan')">
         <img :src="caravan" class="w-[45px] h-[45px] block dark:hidden" />
         <img
-    :src="darkcaravan"
-    class="w-[45px] h-[45px] hidden dark:block"
-  />
+          :src="darkcaravan"
+          class="w-[45px] h-[45px] hidden dark:block"
+        />
         <span class="text-sm mt-[5px] text-[var(--white)]">카라반</span>
       </button>
       <button class="flex flex-col items-center" @click="emit('categoryClick', 'pet')">
@@ -72,10 +75,6 @@ const emit = defineEmits(['filterClick', 'categoryClick'])
         <img :src="darkpet" class="w-[45px] h-[45px] hidden dark:block" />
         <span class="text-sm mt-[5px] text-[var(--white)]">반려동물</span>
       </button>
-      <!-- <button class="flex flex-col items-center" @click="emit('categoryClick', 'chatbot')">
-        <img :src="chatbot" class="w-[45px] h-[45px]" />
-        <span class="text-sm mt-[5px] text-[var(--white)]">챗봇</span>
-      </button> -->
     </section>
   </header>
 </template>
